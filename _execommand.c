fa3b6c2d4f207542b590b92c1cfcb53591fb9a4f
peter precious <peterprecious370@gmail.com>
2023-10-18 06:46:03 -0700

    shell p

---
+++ _execommand.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * execommand - a function to execute command
+ * @argument_v: argument to be executed
+ * Return: void
+ */
+
+void execommand(char **argument_v)
+{
+	char *cmd = NULL, *actual_cmd = NULL;
+	pid_t processID, Wwpid;
+	int stats;
+
+	processID = fork();
+
+	if (processID == 0)
+	{
+		if (argument_v)
+		{
+			cmd = argument_v[0];
+			actual_cmd = getLocation(cmd);
+			if (execve(actual_cmd, argument_v, NULL) == -1)
+			{
+				perror("Error: ");
+			};
+			exit(-1);
+		}
+	}
+	else if (processID < 0)
+	{
+		perror("Error: ");
+	}
+	else
+	{
+		do {
+			Wwpid = waitpid(processID, &stats, WUNTRACED);
+		} while (!WIFEXITED(stats) && !WIFSIGNALED(stats));
+	}
+}
--- _shell.c
+++ _shell.c
@@ -1,12 +1,66 @@
-#include <stdio.h>
+#include "shell.h"
 
 /**
- * main - a function to create  a shekk
+ * main - a function to create  a shell
+ * @argument_c: arg c
+ * @argument_v: arg v
  * Return: 0 if success
  */
 
-int main(void)
+int main(int argument_c, char **argument_v)
 {
-	printf("Hello shell!\n");
+	char *prompt_s = "$ ";
+	char *linepointer;
+	size_t num = 0;
+	ssize_t numchars_read;
+	char *linepointer_c = NULL;
+	const char *delimeter = " \n";
+	int number_token = 0;
+	char *token;
+	int j;
+
+	(void)argument_c;
+
+	while (1)
+	{
+		printf("%s", prompt_s);
+		numchars_read = getline(&linepointer, &num, stdin);
+		if (numchars_read == -1)
+		{
+			printf("Exit...\n");
+			return (-1);
+		}
+
+		linepointer_c = malloc(sizeof(char) * numchars_read);
+		if (linepointer_c == NULL)
+		{
+			perror("Failed to allocate memory...");
+			return (-1);
+		}
+
+		strcpy(linepointer_c, linepointer);
+		token = strtok(linepointer, delimeter);
+
+		while (token != NULL)
+		{
+			number_token++;
+			token = strtok(NULL, delimeter);
+		}
+		number_token++;
+
+		argument_v = malloc(sizeof(char *) * number_token);
+		token = strtok(linepointer_c, delimeter);
+		for (j = 0; token != NULL; j++)
+		{
+			argument_v[j] = malloc(sizeof(char) * strlen(token));
+			strcpy(argument_v[j], token);
+
+			token = strtok(NULL, delimeter);
+		}
+		argument_v[j] = NULL;
+		execommand(argument_v);
+	}
+	free(linepointer_c);
+	free(linepointer);
 	return (0);
 }
