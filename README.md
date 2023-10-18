e68a68a2b53ced957516fda2cfd897bb07838550
peter precious <peterprecious370@gmail.com>
2023-10-18 00:55:34 -0700

    betty

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple shell with petepresh007 and Ayande-442
---
+++ _shell.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a function to create  a shekk
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	printf("Hello shell!\n");
+	return (0);
+}
