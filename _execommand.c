eb94a265761d0fe4282e825e4ae42f1e1a513f83
peter precious <peterprecious370@gmail.com>
2023-10-18 08:06:23 -0700

    fixing exec 2

--- _execommand.c
+++ _execommand.c
@@ -8,30 +8,14 @@
 void execommand(char **argument_v)
 {
 	char *cmd = NULL, *actual_cmd = NULL;
-	pid_t processID;
-	int stats;
 
-	processID = fork();
-
-	if (processID == 0)
+	if (argument_v)
 	{
-		if (argument_v)
+		cmd = argument_v[0];
+		actual_cmd = getLocation(cmd);
+		if (execve(actual_cmd, argument_v, NULL) == -1)
 		{
-			cmd = argument_v[0];
-			actual_cmd = getLocation(cmd);
-			if (execve(actual_cmd, argument_v, NULL) == -1)
-			{
-				perror("Error: ");
-			};
-			exit(-1);
+			perror("Error: ");
 		}
-	}
-	else if (processID < 0)
-	{
-		perror("Error: ");
-	}
-	else
-	{
-		wait(&stats);
 	}
 }
